e3ce43a8635c45e05c2b1c19095927451efb7d58
Petter <petter@petterhaggholm.net>
2021-11-11 12:12:34 -0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   entry: './src/index.ts',
-  devtool: isProduction ? false : 'eval',
+  devtool: isProduction ? 'source-map' : 'eval',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
@@ -14,10 +14,7 @@ const config = {
     open: true,
     host: 'localhost',
   },
-  plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
+  plugins: [],
   module: {
     rules: [
       {
@@ -29,9 +26,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
